9652ec7c67bcf11bc16f615b07e1f475a8403d1d
Sandbox Sync <support@getsandbox.com>
2016-09-17 23:26:34 +0000

    Changes

--- stubs/dynamicAssign.js
+++ stubs/dynamicAssign.js
@@ -12,13 +12,13 @@ exports.dynamicAssign = function (IDKey, req)
     else if (req.body.customer.customerType.toUpperCase() == "BUSINESS") 
     {
         applicationMap[IDKey].responseTemplate = "DYNAMIC_business_new";
-        applicationMap[IDKey].nextResponseTemplate = "DYNAMIC_business_existing";
+        //applicationMap[IDKey].nextResponseTemplate = "DYNAMIC_business_existing";
         applicationMap[IDKey].predefinedResponseFound = true;
     } 
     else if (req.body.customer.customerType.toUpperCase() == "PERSONAL GUARANTOR") 
     {
         applicationMap[IDKey].responseTemplate = "DYNAMIC_PG_new";
-        applicationMap[IDKey].nextResponseTemplate = "DYNAMIC_PG_existing";
+        //applicationMap[IDKey].nextResponseTemplate = "DYNAMIC_PG_existing";
         applicationMap[IDKey].predefinedResponseFound = true;
     } 
     else 
