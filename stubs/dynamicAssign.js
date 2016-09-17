7183a2dbff9e8e9705678bba6ce6ceb7f51e4fd5
Sandbox Sync <support@getsandbox.com>
2016-09-17 21:16:26 +0000

    Changes

--- stubs/dynamicAssign.js
+++ stubs/dynamicAssign.js
@@ -5,16 +5,21 @@ exports.dynamicAssign = function (IDKey, req)
 {
     if (req.body.customer.customerType.toUpperCase() == "INDIVIDUAL") 
     {
-        
+        applicationMap[IDKey].responseTemplate = "DYNAMIC_customer_new";
+        applicationMap[IDKey].predefinedResponseFound = false;
     } 
     else if (req.body.customer.customerType.toUpperCase() == "BUSINESS") 
     {
-        
+        applicationMap[IDKey].responseTemplate = "DYNAMIC_business_new";
+        applicationMap[IDKey].predefinedResponseFound = false;
     } 
     else if (req.body.customer.customerType.toUpperCase() == "PERSONAL GUARANTOR") 
     {
-       
-    } else {
+        applicationMap[IDKey].responseTemplate = "DYNAMIC_PG_new";
+        applicationMap[IDKey].predefinedResponseFound = false;
+    } 
+    else 
+    {
         res.json({
             "error": "Customer Type not recognised"
         });
